1719d9912dc544c4580452179df0adb347c20da1
ritika-sharma123 <113575391+ritika-sharma123@users.noreply.github.com>
2025-06-04 16:44:41 +0530

    Branch

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from './components/Header';
+import { makeStyles } from '@mui/styles';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import LoginPage from './pages/Login';
+
+const useStyles = makeStyles({
+  container:{
+    height: '100vh',
+  }
+})
+
+
+function App() {
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/Login" />} />
+        <Route default path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
